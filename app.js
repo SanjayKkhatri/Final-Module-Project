faf690cf93f19e527befa47ca8e454298c7be160
SanjayKkhatri <khatrisanjay095@gmail.com>
2022-06-13 09:24:10 +0500

    FMP

---
+++ app.js
@@ -0,0 +1,14 @@
+// var MenuItems = document.getElementById("MenuItems");
+
+//         MenuItems.style.maxHeight = "0px";
+
+//         function munetoggle(){
+//             if(MenuItems.style.maxHeight == "0px")
+//             {
+//                 MenuItems.style.maxHeight = "200px"
+//             }
+//         else
+//         {
+//             MenuItems.style.maxHeight = "0px"
+//         }
+//     }
